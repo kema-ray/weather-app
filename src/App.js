b291239b24de374b4b58f2ef3914347b3452806b
kema-ray <rachelkemuma99@gmail.com>
2023-09-20 22:25:40 +0300

    create a search input

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React, {useState} from 'react'
 import axios from 'axios';
 
 function App() {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=nyamira&units=imperial&appid=2bb27db57e6cf9d677836a8059878454`
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=2bb27db57e6cf9d677836a8059878454`
+  
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  }
+  
   return (
     <div className="app">
+      <div className='search'>
+        <input
+        value={location}
+        onChange={event =>setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type='text' />
+      </div>
       <div className='container'>
         <div className='top'>
           <div className='location'>
